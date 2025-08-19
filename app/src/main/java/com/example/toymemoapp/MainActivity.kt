ea2e74e2bcee6f6548edadac032cc38a05375abf
Heochan <heochan01@korea.ac.kr>
2025-08-19 12:48:42 +0900

    Refactor: MainActivity - migrate to 'View Binding'

--- app/src/main/java/com/example/toymemoapp/MainActivity.kt
+++ app/src/main/java/com/example/toymemoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.toymemoapp.databinding.ActivityMainBinding
 import com.example.toymemoapp.ui.theme.ToyMemoAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)   // -> View Binding을 사용함에 따라 deprecated
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "View Binding test"
     }
 }
